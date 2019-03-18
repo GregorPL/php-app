90a971541c55dd4764c0f51fd88e3f014029fa34
Grzegorz Gawlik <grzegorzgawlik94@gmail.com>
2019-03-19 00:26:46 +0100

    update

--- includes/resetPassword.inc.php
+++ includes/resetPassword.inc.php
@@ -1,5 +1,5 @@
 <?php
-	$email = $_POST['login'];
+	$email = $_POST['email'];
 	$pass1 = $_POST['newPass1'];
 	$pass2 = $_POST['newPass2'];
 
@@ -8,7 +8,7 @@
 			$_SESSION['error'] = "Wypełnij wszystkie pola";
 			header("Location: ../resetPassword.php?emptyFields");
 		}
-		else if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+		else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
 			$_SESSION['error'] = "Niepoprawny adres email";
 			header("Location: ../resetPassword.php?invalidEmail");
 		}
@@ -19,6 +19,17 @@
 		else{
 			require "db.inc.php";
 			$sql = "SELECT email FROM users WHERE email = ?";
+			$stmt = mysqli_stmt_init($conn);
+			if (!mysqli_stmt_prepare($stmt, $sql)) {
+				header("Location: ../resetPassword.php?sqlStmtError");
+			}
+			else{
+				$hashedPwd = password_hash($pass1, PASSWORD_DEFAULT);
+				$sqlUpdate = "UPDATE users SET password = ? WHERE email = ?";
+				mysqli_stmt_bind_param($stmt, "ss", $hashedPwd, $email);
+				mysqli_stmt_execute($stmt);
+				header("Location: ../index.php?signup=success");
+			}
 		}
 	}
 	else{
